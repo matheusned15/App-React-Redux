30eae0effd30c7a1abb17eec5dc95f15937a1ed7
matheusned15 <matheusned15@gmail.com>
2021-03-05 19:26:46 -0300

    Configurando Redux

--- src/App.js
+++ src/App.js
@@ -1,16 +1,20 @@
 import React from 'react';
 import Routes from './routes';
-import {BrowserRouter} from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
+import { Provider } from 'react-redux';
 
 import Header from './components/Header';
+import store from './store';
 
 export default function App() {
- return (
-   <div>
-     <BrowserRouter>
-      <Header/>
-      <Routes/>
-     </BrowserRouter>
-   </div>
- );
+  return (
+    <div>
+      <Provider store={store}>
+        <BrowserRouter>
+          <Header />
+          <Routes />
+        </BrowserRouter>
+      </Provider>
+    </div>
+  );
 }
