21944d349376ad7609eeb49fea403595662d6dbd
matheusned15 <matheusned15@gmail.com>
2021-03-07 05:14:43 -0300

    Configurando o Redux Saga

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,52 +1,52 @@
-import React, { useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import { useDispatch } from 'react-redux';
-import { addReserve } from '../../store/modules/reserve/actions';
+import { addReserveRequest } from '../../store/modules/reserve/actions';
 import { MdFlightTakeoff } from 'react-icons/md'
 
 import api from '../../services/api';
 import './style.css';
 
 export default function Home() {
- const dispatch = useDispatch();   
- const [trips, setTrips] = useState([]);
+  const dispatch = useDispatch();
+  const [trips, setTrips] = useState([]);
 
- useEffect(()=>{
+  useEffect(() => {
 
-  async function loadApi(){
-    const response = await api.get('trips');
-    setTrips(response.data);
+    async function loadApi() {
+      const response = await api.get('trips');
+      setTrips(response.data);
+    }
+
+    loadApi();
+
+  }, []);
+
+
+  function handleAdd(id) {
+    dispatch(addReserveRequest(id));
   }
 
-  loadApi();
+  return (
+    <div>
+      <div className="box">
+        {trips.map(trip => (
+          <li key={trip.id}>
+            <img src={trip.image} alt={trip.title} />
+            <strong>{trip.title}</strong>
+            <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
 
- }, []);
-
-
- function handleAdd(trip){
-    dispatch(addReserve(trip));
- }
-
- return (
-   <div>
-     <div className="box">
-       {trips.map(trip => (
-         <li key={trip.id}>
-           <img src={trip.image} alt={trip.title} />
-           <strong>{trip.title}</strong>
-           <span>Status: {trip.status ? 'Disponivel' : 'Indisponivel'}</span>
-
-           <button
-           type="button"
-           onClick={()=> handleAdd(trip) }
-           >  
-             <div>
-               <MdFlightTakeoff size={16} color="#FFF" />
-             </div>
-             <span>SOLICITAR RESERVA</span> 
-           </button>
-         </li>
-       ))}
-     </div>
-   </div>
- );
+            <button
+              type="button"
+              onClick={() => handleAdd(trip.id)}
+            >
+              <div>
+                <MdFlightTakeoff size={16} color="#FFF" />
+              </div>
+              <span>SOLICITAR RESERVA</span>
+            </button>
+          </li>
+        ))}
+      </div>
+    </div>
+  );
 }
