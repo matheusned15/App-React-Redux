32555b2a204a08c20b760c9df80b80d94b2292cc
matheusned15 <matheusned15@gmail.com>
2021-03-07 05:35:55 -0300

    Consultando estoque pt2

--- src/store/modules/reserve/actions.js
+++ src/store/modules/reserve/actions.js
@@ -1,28 +1,36 @@
-export function addReserveRequest(id){
+export function addReserveRequest(id) {
     return {
-      type: 'ADD_RESERVE_REQUEST',
-      id
+        type: 'ADD_RESERVE_REQUEST',
+        id
     }
-  }
-  
-  export function addReserveSuccess(trip){
+}
+
+export function addReserveSuccess(trip) {
     return {
-      type: 'ADD_RESERVE_SUCCESS',
-      trip
+        type: 'ADD_RESERVE_SUCCESS',
+        trip
     }
-  }
-  
-  export function removeReserve(id){
+}
+
+export function removeReserve(id) {
     return {
-      type: 'REMOVE_RESERVE',
-      id,
+        type: 'REMOVE_RESERVE',
+        id,
     }
-  }
-  
-  export function updateAmountReserve(id, amount){
+}
+
+export function updateAmountRequest(id, amount) {
     return {
-      type: 'UPDATE_RESERVE',
-      id,
-      amount
+        type: 'UPDATE_RESERVE_REQUEST',
+        id,
+        amount
     }
-  }
+}
+
+export function updateAmountSuccess(id, amount) {
+    return {
+        type: 'UPDATE_RESERVE_SUCCESS',
+        id,
+        amount
+    }
+}
--- src/store/modules/reserve/sagas.js
+++ src/store/modules/reserve/sagas.js
@@ -1,5 +1,5 @@
 import { select, call, put, all, takeLatest } from 'redux-saga/effects';
-import { addReserveSuccess, updateAmountReserve } from './actions';
+import { addReserveSuccess, updateAmountSuccess } from './actions';
 import api from '../../../services/api';
 
 function* addToReserve({ id }) {
@@ -22,7 +22,7 @@ function* addToReserve({ id }) {
 
   if (tripExists) {
 
-    yield put(updateAmountReserve(id, amount));
+    yield put(updateAmountSuccess(id, amount));
 
   } else {
     const response = yield call(api.get, `trips/${id}`);
@@ -39,6 +39,24 @@ function* addToReserve({ id }) {
 
 }
 
+
+function* updateAmount({ id, amount }) {
+  if (amount <= 0) return;
+
+  const myStock = yield call(api.get, `/stock/${id}`);
+
+  const stockAmount = myStock.data.amount;
+
+  if (amount > stockAmount) {
+    alert('Quantidade maxima atingida.');
+    return;
+  }
+
+  yield put(updateAmountSuccess(id, amount));
+
+}
+
 export default all([
-  takeLatest('ADD_RESERVE_REQUEST', addToReserve)
+  takeLatest('ADD_RESERVE_REQUEST', addToReserve),
+  takeLatest('UPDATE_RESERVE_REQUEST', updateAmount),
 ])
